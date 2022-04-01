0409cc21aa224eb3c6566adaa6309f73b5f6a07d
ThibaudS <thibaud.devpro@gmail.com>
2022-04-01 10:38:17 +0200

    Added apiClient, photographers images, calling the apiClient to index.js - Modify the photographers picture from css on landing page

---
+++ scripts/api/apiClient.js
@@ -0,0 +1,32 @@
+class ApiClient {
+    constructor() {
+        this.url = '../../data/photographers.json'
+    }
+
+    async getDB() {
+        const response = await fetch(this.url)
+        this.handleErrors(response)
+        const result = await response.json()
+        return result
+    }
+
+    async getPhotographers() {
+        const dataBase = await this.getDB()
+        const photographers = dataBase.photographers
+        return photographers
+
+    }
+
+    async getMedias() {
+        const dataBase = await this.getDB()
+        const medias = dataBase.media
+        return medias
+    }
+
+    handleErrors(response) {
+        if (!response.ok) throw new Error(response.status)
+        return response
+    }
+}
+
+export const apiClient = new ApiClient()
