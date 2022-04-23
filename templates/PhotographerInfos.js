da443883ccb66f2ad6e59f4a1177c256acb1ac1e
ThibaudS <thibaud.devpro@gmail.com>
2022-04-23 11:02:53 +0200

    Responsive done

--- templates/PhotographerInfos.js
+++ templates/PhotographerInfos.js
@@ -3,7 +3,7 @@ class photographerInfo {
         return `<article>
 	                <a  href="photographer.html?photographerId=${dataPhotographer.id}">
 		                <img src="assets/photographers/${dataPhotographer.portrait}" alt="${dataPhotographer.name}" />
-		                <h2>${dataPhotographer.name}</h2>
+		                <h2 class="landing-photographer-name">${dataPhotographer.name}</h2>
 	                </a>
 	                <div>
 		                <p>${dataPhotographer.city}, ${dataPhotographer.country}</p>
