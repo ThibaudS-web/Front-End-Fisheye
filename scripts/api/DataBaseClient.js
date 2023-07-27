a989510f8e22ced0ddc2d1646f1a9716189e5c4f
ThibaudS <thibaud.devpro@gmail.com>
2023-07-27 11:31:52 +0200

    Change fetching url

--- scripts/api/DataBaseClient.js
+++ scripts/api/DataBaseClient.js
@@ -1,6 +1,6 @@
 class DataBaseClient {
     constructor() {
-        this.url = '../../data/photographers.json'
+        this.url = 'data/photographers.json'
     }
 
     async getDB() {
@@ -20,7 +20,7 @@ class DataBaseClient {
         const dataBase = await this.getDB()
         const medias = dataBase?.media
         return medias
-    } 
+    }
 
     handleErrors(response) {
         if (!response.ok) throw new Error(response.status)
